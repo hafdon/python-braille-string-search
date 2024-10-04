68d3cc692731903351fe7c0779b4628ed8304e8a
hafdon <zweisser@gmail.com>
2024-10-04 00:56:51 -0700

    feat: update find_matches to carry forward DOTS info

--- main.py
+++ main.py
@@ -52,12 +52,11 @@ def compile_patterns(array_of_lists):
 # Function to find matches in a line for a specific list
 def find_matches(line, compiled_list):
     matches = []
-    for word, pattern in compiled_list["patterns"]:
+    for word, pattern, dots in compiled_list["patterns"]:
         for match in pattern.finditer(line):
             start = match.start()
             matched_text = match.group(0)
-            matches.append((start, matched_text, word))
-    # Sort matches by starting index
+            matches.append((start, matched_text, word, dots))
     matches.sort(key=lambda x: x[0])
     return matches
 
